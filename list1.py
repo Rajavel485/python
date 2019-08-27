4e2986e3c5591134f8f4c406d98128213b06bed4
Rajavel <54536603+Rajavel485@users.noreply.github.com>
2019-08-27 14:07:55 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,10 @@
+value="sherluck"
+new_list=[56,90,8,8,76]
+print(new_list.pop())
+print(new_list)
+strings="i am stuying in paavai"
+print(strings.count('a'))
+print(strings.index('a',3))
+mynew_list=strings.split()
+print(mynew_list)
+
